bd4e46c404f059e37ba4f0b24189154077304633
Axel Lemaire <axel.lemaire@epitech.eu>
2023-11-30 18:42:53 +0100

    [+] : init Cmake for macos

--- client/main.cpp
+++ client/main.cpp
@@ -5,7 +5,31 @@
 ** main.cpp
 */
 
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 int main(int ac, char **av)
 {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test Window");
+
+    // Main loop that continues until the window is closed
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window close event
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        // Clear the window with a black color
+        window.clear(sf::Color::Black);
+
+        // You can add your drawing code here
+
+        // Display what has been drawn to the window
+        window.display();
+    }
+
     return 0;
 }
